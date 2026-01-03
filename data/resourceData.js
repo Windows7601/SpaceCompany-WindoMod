d15f81d08ced5a70b9a6d514aaf050c0c2324fd1
Windows7601 <dursannebis.ekranbir@gmail.com>
2026-01-03 23:19:00 +0300

    Update resourceData.js
    Steel

--- data/resourceData.js
+++ data/resourceData.js
@@ -87,6 +87,15 @@ Game.resourceData = (function () {
         unlocked: false
     };
 
+    instance.steel = {
+        name: 'Steel',
+        desc: 'Steel is very durable, but also hard to acquire.',
+        icon: 'metalIcon',
+        category: 'earth',
+        baseCapacity: 50,
+        unlocked: true
+    };
+
     instance.metal = {
         name: 'Metal',
         desc: 'Metal is one of the primary resources. It is used for many things, including storage upgrades, machinery and most things in space.',
